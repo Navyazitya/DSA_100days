96991aa180442941ea6cb09c4d0b1ee8798c5488
Navyazitya <151536566+Navyazitya@users.noreply.github.com>
2024-05-20 18:44:45 +0530

    Delete demo.java

--- demo.java
+++
@@ -1,8 +0,0 @@
-public class demo{
-    public static void main(String args[]){
-        System.out.print("Hello DSA!");
-        System.out.println("Hey! we are in next line!");
-        System.out.println("Hey! we are in next line!");
-        System.out.print("Trying new \n things");
-    }
-}
