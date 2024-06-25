42c8aaf7ec7962298d642587bec06ffef49f34e7
Navya Zitya Pottipochala <151536566+Navyazitya@users.noreply.github.com>
2024-06-25 06:02:57 +0530

    Day26
    Day26 of DSA with java.

---
+++ Day26/anagramfreq.java
@@ -0,0 +1,35 @@
+public class anagramfreq {
+    
+    public static boolean areAnagrams(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        int[] frequencyArray1 = new int[26];
+        int[] frequencyArray2 = new int[26];
+
+        for (int i = 0; i < str1.length(); i++) {
+            frequencyArray1[str1.charAt(i) - 'a']++;
+            frequencyArray2[str2.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (frequencyArray1[i] != frequencyArray2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println(str1 + " and " + str2 + " are anagrams.");
+        } else {
+            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+        }
+    }
+}
